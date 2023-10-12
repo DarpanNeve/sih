cd847d93f89d50b8230a5dea6b762bbc729bcb02
Darpan Neve <darpanneve3@gmail.com>
2023-10-12 21:50:47 +0530

    Login Page Done

---
+++ lib/Api/auth_service.dart
@@ -0,0 +1,14 @@
+
+
+import 'package:flutter/src/widgets/framework.dart';
+
+class AuthService{
+  void signInWithEmailAndPassword(String email, String pass,context){
+
+  }
+
+  void signInWithGoogle(BuildContext context) {
+
+  }
+
+}
---
+++ lib/Pages/LoginPage.dart
@@ -0,0 +1,236 @@
+import 'package:SIH/Pages/forgot_password.dart';
+import 'package:SIH/Pages/registration_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+import '../Api/auth_service.dart';
+
+var _pass = TextEditingController();
+var _email = TextEditingController();
+bool _obsecureText = true;
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              const SizedBox(
+                height: 60,
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              SizedBox(
+                width: 344,
+                height: 500,
+                child: Column(
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.all(12.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Text(
+                            'LOGIN ',
+                            style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 25,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: TextField(
+                        controller: _email,
+                        decoration: const InputDecoration(
+                          hintText: 'Email',
+                          prefixIcon: Icon(Icons.email),
+                        ),
+                        keyboardType: TextInputType.emailAddress,
+                      ),
+                    ),
+                    const PasswordButton(),
+                    const SizedBox(
+                      height: 25,
+                    ),
+                    const SizedBox(
+                      height: 4,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => const ForgotPassword()));
+                            },
+                            child: const Text(
+                              'Forgot Password ?',
+                              style: TextStyle(color: Colors.lightBlue),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 4,
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        AuthService().signInWithEmailAndPassword(
+                            _email.text.toString(),
+                            _pass.text.toString(),
+                            context);
+                      },
+                      child: Container(
+                        width: RenderErrorBox.minimumWidth,
+                        height: 45,
+                        decoration: BoxDecoration(
+                          color: Colors.lightBlue,
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        child: const Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              "Login",
+                              style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 25,
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        AuthService().signInWithGoogle(context);
+                      },
+                      child: Container(
+                        width: RenderErrorBox.minimumWidth,
+                        height: 45,
+                        decoration: BoxDecoration(
+                          color: Colors.lightBlue,
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            SizedBox(
+                                width: 40,
+                                height: 40,
+                                child: Image.asset(
+                                    'assets/Images/Google.png')),
+                            const Text(
+                              "Google",
+                              style: TextStyle(
+                                  fontSize: 16,
+                                  // Set the text size
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    const Text('OR'),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    const Text("Don't Have An Account ? "),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const RegistrationPage(),
+                          ),
+                        );
+                      },
+                      child: const Text(
+                        "Register Now ",
+                        textWidthBasis: TextWidthBasis.longestLine,
+                        style: TextStyle(
+                            color: Colors.lightBlue, fontSize: 16),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 4,
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class PasswordButton extends StatefulWidget {
+  const PasswordButton({super.key});
+
+  @override
+  State<PasswordButton> createState() => _PasswordButtonState();
+}
+
+class _PasswordButtonState extends State<PasswordButton> {
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _email.text = "";
+    _pass.text = "";
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(12.0),
+      child: TextField(
+        controller: _pass,
+        obscureText: _obsecureText,
+        decoration: InputDecoration(
+          hintText: 'Password',
+          prefixIcon: const Icon(Icons.lock),
+          suffixIcon: GestureDetector(
+              onTap: () {
+                setState(
+                      () {
+                    _obsecureText = !_obsecureText;
+                  },
+                );
+              },
+              child: _obsecureText
+                  ? const Icon(Icons.visibility_off, color: Colors.grey)
+                  : const Icon(Icons.visibility, color: Colors.grey)),
+        ),
+        keyboardType: TextInputType.emailAddress,
+      ),
+    );
+  }
+}
