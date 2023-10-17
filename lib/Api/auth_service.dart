14bdc178c2e2d66c6ecd2bf7cc1f87e6afabb6f6
Darpan Neve <darpanneve3@gmail.com>
2023-10-17 08:45:51 +0530

    Login Page Done

--- lib/Api/auth_service.dart
+++ lib/Api/auth_service.dart
@@ -1,14 +1,132 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+// import 'package:jaldindi/Auth/login_page.dart';
+// import 'package:jaldindi/Home/home_page.dart';
+// import 'package:jaldindi/Home/Admin/admin.dart';
+import '../Pages/LoginPage.dart';
+import '../Widgets/Widget.dart';
 
-
-import 'package:flutter/src/widgets/framework.dart';
-
-class AuthService{
-  void signInWithEmailAndPassword(String email, String pass,context){
-
+class AuthService {
+  handleAuthState() {
+    return StreamBuilder(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (BuildContext context, AsyncSnapshot<User?> snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        } else if (snapshot.hasData) {
+          if (FirebaseAuth.instance.currentUser!.emailVerified) {
+            // checkIsAdmin();
+            return const LoginPage();
+          } else {
+            signOutWithoutSnackBar();
+            sendVerificationEmail(context);
+            return const LoginPage();
+          }
+// Navigate to the home page.
+        } else {
+          // User is not signed in.
+          return const LoginPage(); // Navigate to the login page.
+        }
+      },
+    );
   }
 
-  void signInWithGoogle(BuildContext context) {
-
+  signInWithGoogle(BuildContext context) async {
+    if (kIsWeb) {
+      GoogleAuthProvider googleProvider = GoogleAuthProvider();
+      googleProvider
+          .addScope('https://www.googleapis.com/auth/contacts.readonly');
+      googleProvider.setCustomParameters({'login_hint': 'user@example.com'});
+      return await FirebaseAuth.instance.signInWithPopup(googleProvider);
+    } else if (defaultTargetPlatform == TargetPlatform.android ||
+        defaultTargetPlatform == TargetPlatform.iOS) {
+      // Trigger the authentication flow
+      final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+      // Obtain the auth details from the request
+      final GoogleSignInAuthentication? googleAuth =
+          await googleUser?.authentication;
+      // Create a new credential
+      final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth?.accessToken,
+        idToken: googleAuth?.idToken,
+      );
+      // Once signed in, return the UserCredential
+      return await FirebaseAuth.instance.signInWithCredential(credential);
+    } else {
+      showSnackBar(
+          "Platform do not support login", context, Icons.error, Colors.red);
+    }
   }
 
+  signOut(BuildContext context) async {
+    if (defaultTargetPlatform == TargetPlatform.android ||
+        defaultTargetPlatform == TargetPlatform.iOS) {
+      await GoogleSignIn().signOut();
+    }
+    await FirebaseAuth.instance.signOut();
+    if (context.mounted) {
+      showSnackBar("signed out.", context, Icons.done, Colors.green);
+    }
+  }
+
+  createUserWithEmailAndPassword(String name, String emailAddress,
+      String password, BuildContext context) async {
+    try {
+      await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: emailAddress,
+        password: password,
+      );
+
+      await FirebaseAuth.instance.currentUser!.updateDisplayName(name);
+      await FirebaseAuth.instance.currentUser!.sendEmailVerification();
+      if (context.mounted) {
+        showSnackBar(
+            "Verification Email send", context, Icons.done, Colors.green);
+      }
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password' && context.mounted ) {
+        showSnackBar(e.code, context, Icons.error, Colors.red);
+      } else if (e.code == 'email-already-in-use' && context.mounted) {
+        showSnackBar(e.code, context, Icons.error, Colors.red);
+      }
+    } catch (e) {
+     if(context.mounted){
+       showSnackBar(e.toString(), context, Icons.error, Colors.red);
+     }
+    }
+  }
+
+  signInWithEmailAndPassword(
+      String emailAddress, String password, BuildContext context) async {
+    try {
+      await FirebaseAuth.instance
+          .signInWithEmailAndPassword(email: emailAddress, password: password);
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        showSnackBar("Please Register Before Logging In", context, Icons.error,
+            Colors.red);
+      } else if (e.code == 'wrong-password') {
+        showSnackBar(
+            "Please Enter Correct Password", context, Icons.error, Colors.red);
+      }
+    }
+  }
+
+  signOutWithoutSnackBar() async {
+    if (defaultTargetPlatform == TargetPlatform.android ||
+        defaultTargetPlatform == TargetPlatform.iOS) {
+      await GoogleSignIn().signOut();
+    }
+    await FirebaseAuth.instance.signOut();
+  }
+
+  sendVerificationEmail(BuildContext context) async {
+    await FirebaseAuth.instance.currentUser!.sendEmailVerification();
+    if (context.mounted) {
+      showSnackBar(
+          "Verification email send", context, Icons.done, Colors.green);
+    }
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:SIH/Pages/LoginPage.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MaterialApp(home:  LoginPage()));
 }
 
